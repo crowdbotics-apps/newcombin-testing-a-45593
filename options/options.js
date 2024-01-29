52ce5088bdb6f44662c354179ef0d45401fdf2fd
Nicolas Finelli <nicolas.finelli@crowdbotics.com>
2024-01-29 12:46:12 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "newcombin-testing-a-45593",
   url: "https://newcombin-testing-a-45593.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
